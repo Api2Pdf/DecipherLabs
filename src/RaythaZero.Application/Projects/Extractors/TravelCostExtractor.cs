9b98aeafd653a61786b11d8aa44b24166e3d9cad
Zack Schwartz <zack@getopenwater.com>
2025-01-24 20:16:09 -0600

    Update TravelCostExtractor.cs

--- src/RaythaZero.Application/Projects/Extractors/TravelCostExtractor.cs
+++ src/RaythaZero.Application/Projects/Extractors/TravelCostExtractor.cs
@@ -39,7 +39,8 @@ public class TravelCostExtractor : AbstractExtractor
         chatHistory.AddUserMessage(renderedTravelCostPrompt);
         var travelCostResponse = await _aiService.GetResponse(chatHistory);
 
-        return travelCostResponse;
+        var responseText = travelCostResponse.First().Content;
+        return responseText;
     }
 
     private string GetTravelCostJsonSchema()
